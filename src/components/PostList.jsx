f5fdfb6e342cbbb9a602716ce91ed13cbce1a209
Vova <86399820+vov4ikgod@users.noreply.github.com>
2022-09-11 22:07:32 +0500

    Add files via upload

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,26 +1,26 @@
-import React from "react";
-import Post from "./Post";
-import { TransitionGroup, CSSTransition } from '../../node_modules/react-transition-group/';
+// import React from "react";
+// import Post from "./Post";
+// import { TransitionGroup, CSSTransition } from '../../node_modules/react-transition-group/';
 
-const PostList = ({posts, title, remove}) => {
-    if (!posts.length) {
-        return (
-            <h1 style={{textAlign: 'center'}}>Посты не найдены!</h1>
-        )
-    }
+// const PostList = ({posts, title, remove}) => {
+//     if (!posts.length) {
+//         return (
+//             <h1 style={{textAlign: 'center'}}>Посты не найдены!</h1>
+//         )
+//     }
 
-    return (
-        <div>
-            <h1 style={{textAlign: 'center'}}>{title}</h1>
-            <TransitionGroup>
-                {posts.map((post, index) =>
-                    <CSSTransition key={post.id} timeout={500} classNames="post">
-                        <Post remove={remove} number={index + 1} post={post}/>
-                    </CSSTransition>
-                )}
-            </TransitionGroup>
-        </div>
-    );
-};
+//     return (
+//         <div>
+//             <h1 style={{textAlign: 'center'}}>{title}</h1>
+//             <TransitionGroup>
+//                 {posts.map((post, index) =>
+//                     <CSSTransition key={post.id} timeout={500} classNames="post">
+//                         <Post remove={remove} number={index + 1} post={post}/>
+//                     </CSSTransition>
+//                 )}
+//             </TransitionGroup>
+//         </div>
+//     );
+// };
 
-export default PostList
+// export default PostList
